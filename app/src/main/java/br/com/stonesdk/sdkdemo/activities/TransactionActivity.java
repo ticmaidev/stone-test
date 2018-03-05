ed244efcddc8e2e35f2143a96b6aff15c4843c72
Felipe Rodrigues <filpgame@gmail.com>
2018-03-05 16:42:27 -0300

    using transactionObject instead of the deleted StoneTransaction

--- app/src/main/java/br/com/stonesdk/sdkdemo/activities/TransactionActivity.java
+++ app/src/main/java/br/com/stonesdk/sdkdemo/activities/TransactionActivity.java
@@ -25,10 +25,10 @@ import stone.application.enums.TransactionStatusEnum;
 import stone.application.enums.TypeOfTransactionEnum;
 import stone.application.interfaces.StoneActionCallback;
 import stone.application.interfaces.StoneCallbackInterface;
+import stone.database.transaction.TransactionObject;
 import stone.providers.LoadTablesProvider;
 import stone.providers.TransactionProvider;
 import stone.utils.Stone;
-import stone.utils.StoneTransaction;
 
 public class TransactionActivity extends AppCompatActivity {
 
@@ -79,35 +79,33 @@ public class TransactionActivity extends AppCompatActivity {
         sendButton.setOnClickListener(new OnClickListener() {
             public void onClick(View v) {
 
-                // Cria o objeto de transacao. Usar o "Stone.getPinpadFromListAt"
-                // significa que devera estar conectado com ao menos um pinpad, pois o metodo
-                // cria uma lista de conectados e conecta com quem estiver na posicao "0".
-                StoneTransaction stoneTransaction = new StoneTransaction(Stone.getPinpadFromListAt(0));
+                // Cria o objeto de transacao.
+                final TransactionObject transaction = new TransactionObject();
 
                 // A seguir deve-se popular o objeto.
-                stoneTransaction.setAmount(valueEditText.getText().toString());
-                stoneTransaction.setEmailClient(null);
-                stoneTransaction.setUserModel(Stone.getUserModel(0));
-                stoneTransaction.setSignature(BitmapFactory.decodeResource(getResources(), R.drawable.signature));
-                stoneTransaction.setCapture(captureTransactionCheckBox.isChecked());
+                transaction.setAmount(valueEditText.getText().toString());
+                //Caso o cartão necessite assinatura, inserir o bitmap da assinatura aqui.
+                transaction.setSignature(BitmapFactory.decodeResource(getResources(), R.drawable.signature));
+                //Transação deve ser feita com captura imediata ou posterior
+                transaction.setCapture(captureTransactionCheckBox.isChecked());
 
                 // AVISO IMPORTANTE: Nao e recomendado alterar o campo abaixo do
                 // ITK, pois ele gera um valor unico. Contudo, caso seja
                 // necessario, faca conforme a linha abaixo.
-//                stoneTransaction.setInitiatorTransactionKey("SEU_IDENTIFICADOR_UNICO_AQUI");
+//                transaction.setInitiatorTransactionKey("SEU_IDENTIFICADOR_UNICO_AQUI");
 
                 // Informa a quantidade de parcelas.
-                stoneTransaction.setInstalmentTransactionEnum(InstalmentTransactionEnum.getAt(instalmentsSpinner.getSelectedItemPosition()));
+                transaction.setInstalmentTransaction(InstalmentTransactionEnum.getAt(instalmentsSpinner.getSelectedItemPosition()));
 
                 // Verifica a forma de pagamento selecionada.
                 if (debitRadioButton.isChecked()) {
-                    stoneTransaction.setTypeOfTransaction(TypeOfTransactionEnum.DEBIT);
+                    transaction.setTypeOfTransaction(TypeOfTransactionEnum.DEBIT);
                 } else {
-                    stoneTransaction.setTypeOfTransaction(TypeOfTransactionEnum.CREDIT);
+                    transaction.setTypeOfTransaction(TypeOfTransactionEnum.CREDIT);
                 }
 
                 // Processo para envio da transacao.
-                final TransactionProvider provider = new TransactionProvider(TransactionActivity.this, stoneTransaction, Stone.getPinpadFromListAt(0));
+                final TransactionProvider provider = new TransactionProvider(TransactionActivity.this, transaction, Stone.getUserModel(0), Stone.getPinpadFromListAt(0));
                 provider.useDefaultUI(false);
                 provider.setDialogMessage("Enviando..");
                 provider.setDialogTitle("Aguarde");
@@ -119,7 +117,7 @@ public class TransactionActivity extends AppCompatActivity {
                     }
 
                     public void onSuccess() {
-                        if (provider.getTransactionStatus() == TransactionStatusEnum.APPROVED) {
+                        if (transaction.getTransactionStatus() == TransactionStatusEnum.APPROVED) {
                             Toast.makeText(getApplicationContext(), "Transação enviada com sucesso e salva no banco. Para acessar, use o TransactionDAO.", Toast.LENGTH_SHORT).show();
                         } else {
                             Toast.makeText(getApplicationContext(), "Erro na transação: \"" + provider.getMessageFromAuthorize() + "\"", Toast.LENGTH_LONG).show();
@@ -130,7 +128,7 @@ public class TransactionActivity extends AppCompatActivity {
                     public void onError() {
                         Toast.makeText(getApplicationContext(), "Erro na transação", Toast.LENGTH_SHORT).show();
                         if (provider.theListHasError(ErrorsEnum.NEED_LOAD_TABLES)) { // code 20
-                            LoadTablesProvider loadTablesProvider = new LoadTablesProvider(TransactionActivity.this, Stone.getPinpadFromListAt(0), Stone.getUserModel(0));
+                            LoadTablesProvider loadTablesProvider = new LoadTablesProvider(TransactionActivity.this, Stone.getPinpadFromListAt(0));
                             loadTablesProvider.setDialogMessage("Subindo as tabelas");
                             loadTablesProvider.useDefaultUI(false); // para dar feedback ao usuario ou nao.
                             loadTablesProvider.execute();
