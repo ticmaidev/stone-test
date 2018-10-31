4ba17970789b2705da34859f178c58a072689acd
TiagoCabral <tiago.gnr1985@gmail.com>
2018-10-31 19:50:15 -0300

    Deletes NEED_LOAD_TABLES error.

--- app/src/main/java/br/com/stonesdk/sdkdemo/activities/TransactionActivity.java
+++ app/src/main/java/br/com/stonesdk/sdkdemo/activities/TransactionActivity.java
@@ -2,10 +2,7 @@ package br.com.stonesdk.sdkdemo.activities;
 
 import android.widget.Toast;
 
-import stone.application.enums.ErrorsEnum;
 import stone.application.enums.TransactionStatusEnum;
-import stone.application.interfaces.StoneCallbackInterface;
-import stone.providers.LoadTablesProvider;
 import stone.providers.TransactionProvider;
 import stone.utils.Stone;
 
@@ -21,27 +18,21 @@ public class TransactionActivity extends BaseTransactionActivity<TransactionProv
         if (transactionObject.getTransactionStatus() == TransactionStatusEnum.APPROVED) {
             Toast.makeText(getApplicationContext(), "Transação enviada com sucesso e salva no banco. Para acessar, use o TransactionDAO.", Toast.LENGTH_SHORT).show();
         } else {
-            Toast.makeText(getApplicationContext(), "Erro na transação: \"" + getAuthorizationMessage() + "\"", Toast.LENGTH_LONG).show();
+
+            String msg = "Erro na transação";
+
+            if (getAuthorizationMessage() == null) {
+                msg += ":" + getAuthorizationMessage();
+            } else {
+                msg += "!";
+            }
+
+            Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_LONG).show();
         }
     }
 
     @Override
-    public void onError()   {
+    public void onError() {
         super.onError();
-        if (providerHasErrorEnum(ErrorsEnum.NEED_LOAD_TABLES)) { // code 20
-            LoadTablesProvider loadTablesProvider = new LoadTablesProvider(TransactionActivity.this, Stone.getPinpadFromListAt(0));
-            loadTablesProvider.setDialogMessage("Subindo as tabelas");
-            loadTablesProvider.useDefaultUI(true); // para dar feedback ao usuario ou nao.
-            loadTablesProvider.execute();
-            loadTablesProvider.setConnectionCallback(new StoneCallbackInterface() {
-                public void onSuccess() {
-                    initTransaction(); // reinicia a transação
-                }
-
-                public void onError() {
-                    Toast.makeText(getApplicationContext(), "Sucesso.", Toast.LENGTH_SHORT).show();
-                }
-            });
-        }
     }
 }
